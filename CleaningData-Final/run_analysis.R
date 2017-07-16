522db16de0f7fa0c81aef62706ff1446a604232d
Dima Kovalenko <dima.kovalenko@uptech.team>
2017-07-16 15:50:17 +0300

    Download raw data from the internet

--- CleaningData-Final/run_analysis.R
+++ CleaningData-Final/run_analysis.R
@@ -1,10 +1,10 @@
 library(data.table)
 library(dplyr)
 
-#temp <- tempfile()
-#download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",temp)
-#unzip(temp)
-#unlink(temp)
+temp <- tempfile()
+download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",temp)
+unzip(temp)
+unlink(temp)
 
 # Read data
 trainX <- fread("UCI HAR Dataset/train/X_train.txt")
